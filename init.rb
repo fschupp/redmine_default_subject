49ec5970f2372370eb7197fa4966f149babd46bd
Felix Schupp <fschupp@outlook.com>
2015-01-25 21:34:46 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,13 +2,14 @@ require 'redmine'
 
 require 'redmine_default_subject/hooks/default_subject_issues_hooks.rb'
 
-project_module :redmine_default_subject do
-  # nothing to do
-end
-
 Redmine::Plugin.register :redmine_default_subject do
   name 'Default Subject Plugin'
   author 'Felix Schupp'
-  description 'Set the subject of an issue to "No subject assigned".'
+  description 'Set the subject of an issue to "<No subject>".'
   version '0.1'
+  
+  project_module :redmine_default_subject do
+  # nothing to do
+end
+
 end
