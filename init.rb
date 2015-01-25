b6bc702e16de3b5d0a258201269cd39bea91317f
Felix Schupp <fschupp@outlook.com>
2015-01-25 21:31:02 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@ require 'redmine'
 
 require 'redmine_default_subject/hooks/default_subject_issues_hooks.rb'
 
+project_module :redmine_default_subject
+
 Redmine::Plugin.register :redmine_default_subject do
   name 'Default Subject Plugin'
   author 'Felix Schupp'
